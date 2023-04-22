0a0ccb55122a577b746e71c233372c713b18467c
Jeremy Cowgar <jeremy@cowgar.com>
2023-04-22 16:23:22 -0400

    feat(tmux): available only when TMUX_PANE is set, also user configurable pass/fail style

--- lib/ex_unit_notifier/notifiers/tmux_notifier.ex
+++ lib/ex_unit_notifier/notifiers/tmux_notifier.ex
@@ -1,22 +1,25 @@
 defmodule ExUnitNotifier.Notifiers.TmuxNotifier do
   @moduledoc false
 
-  def notify(status, _message, _opts) do
-    pane = System.get_env("TMUX_PANE")
-
-    if pane do
-      # tmux set-window-option -t"$TMUX_PANE" window-status-style bg=red
-      System.cmd("tmux", ["set-window-option", "-t#{pane}", "window-status-style", style(status)])
-    end
+  def notify(status, _message, opts) do
+    # tmux set-window-option -t"$TMUX_PANE" window-status-style bg=red
+    System.cmd("tmux", [
+      "set-window-option",
+      "-t#{tmux_pane()}",
+      "window-status-style",
+      style(status, opts)
+    ])
   end
 
-  def available?, do: true
+  def available?, do: tmux_pane() != nil
 
-  defp style(status) do
-    if status == :error do
-      "bg=red"
-    else
-      "bg=green"
-    end
+  defp style(:ok, opts) do
+    Map.get(opts, :tmux_pass_style, "bg=green")
   end
+
+  defp style(:error, opts) do
+    Map.get(opts, :tmux_fail_style, "bg=red")
+  end
+
+  defp tmux_pane, do: System.get_env("TMUX_PANE")
 end
